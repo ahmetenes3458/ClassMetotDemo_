b1e3ad2fb969b500426255728bbf7392054bca12
User <User@LAPTOP-K1BB8HNP>
2022-04-28 01:37:43 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri Eklendi : " + musteri.Ad + " " + musteri.Soyad);
+        }
+        public void Sil(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri Sil : " + musteri.Ad + " " + musteri.Soyad);
+        }
+
+        public void Listele(Musteri musteri)
+        {
+            Console.WriteLine("Müşteriler Listelendi : ");
+            Console.WriteLine(musteri.Ad + " " + musteri.Soyad);
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,26 @@
+﻿using ClassMetotDemo;
+
+Musteri musteri1 = new Musteri();
+musteri1.Id = 1;
+musteri1.Ad = "Ali";
+musteri1.Soyad = "Candan";
+musteri1.DogumYili = 2001;
+
+Musteri musteri2 = new Musteri();
+musteri2.Id = 2;
+musteri2.Ad = "Aslan";
+musteri2.Soyad = "Akbey";
+musteri2.DogumYili = 1972;
+
+Musteri[] musteriler = new Musteri[] { musteri1, musteri2 };
+
+MusteriManager musteriManager1 = new MusteriManager();
+musteriManager1.Ekle(musteri1);
+musteriManager1.Ekle(musteri2);
+
+foreach (var musteri in musteriler)
+{
+    musteriManager1.Listele(musteri);
+}
+
+musteriManager1.Sil(musteri2);
